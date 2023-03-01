fd574a6748603c52a84eaf77f4e8318987920f56
Kasia Półrolniczak <k.polrolniczak@gmail.com>
2023-03-01 10:31:20 +0100

    adding addToCart reducer

---
+++ src/store/cart-slice.js
@@ -0,0 +1,39 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        itemsList: [],
+        totalQuantity: 0,
+        showCart: false
+    },
+    reducers: {
+        addToCart(state, action) {
+            const newItem = action.payload
+            //to check if the item is available, if the item id is in the itemList
+            const existingItem = state.itemsList.find((item) => item.id === newItem.id)
+            //if the existing product is available we can increase quantity
+            if (existingItem) {
+                existingItem.quantity++
+                existingItem.price += newItem.price
+            } else {
+                state.itemsList.push({
+                    id: newItem.id,
+                    price: newItem.price,
+                    quantity: 1,
+                    totalPrice: newItem.price,
+                    name: newItem.name
+                })
+            }
+        },
+        removeFromCart() {
+
+        },
+        setShowCart(state) {
+            state.showCart = true
+        }
+    }
+})
+
+export const cartActions = cartSlice.actions;
+export default cartSlice;
